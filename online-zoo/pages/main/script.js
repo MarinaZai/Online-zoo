dd1d6251856f7d90f50ab84662fe2bc6b2cc3aa9
MarinaZai <Marintos34@gmail.com>
2022-09-20 16:35:24 +0300

    fix: change interactive on landing

--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -8,3 +8,14 @@ for (let i = 0; i < links.length; i++) {
     this.className += " active";
   });
 }
+
+//Footer nav links change active
+const linkContainerFooter = document.getElementsByClassName("nav_footer");
+const linksFooter = linkContainerFooter[0].getElementsByClassName("link_footer");
+for (let i = 0; i < linksFooter.length; i++) {
+  linksFooter[i].addEventListener("click", function() {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+} 
